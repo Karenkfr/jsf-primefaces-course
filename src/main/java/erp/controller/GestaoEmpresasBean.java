92609516b124749cc3088e049b482a7950579d42
Ribeiro <karen.ribeiro@capgemini.com>
2023-08-28 15:48:10 -0300

    conversor customizado implantado

--- src/main/java/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/erp/controller/GestaoEmpresasBean.java
@@ -1,60 +1,79 @@
 package erp.controller;
 
-
 import java.io.Serializable;
 import java.util.List;
 
+import javax.faces.convert.Converter;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import erp.model.Empresa;
+import erp.model.RamoAtividade;
 import erp.model.TipoEmpresa;
 import erp.repository.EmpresasRepository;
+import erp.repository.RamoAtividadesRepository;
 import erp.util.FacesMensages;
-
 
 @Named
 @ViewScoped
 public class GestaoEmpresasBean implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    
-    @Inject
-    private EmpresasRepository empresas;
-    
-    private List<Empresa> listaEmpresas;
-    
-    private String termoPesquisa;
-    
-    @Inject
-    private FacesMensages facesMensages;
-    
-    public void pesquisar() {
-    	listaEmpresas = empresas.pesquisar(termoPesquisa);
-    	
-    	if(listaEmpresas.isEmpty()) {
-    		facesMensages.info("Sua consulta não retornou registros.");
-    		
-    	}
-    }
-    
-    public void todasEmpresas() {
-        //listaEmpresas = empresas.todas();
-    }
-    
-    public List<Empresa> getListaEmpresas() {
-        return listaEmpresas;
-    }
-    
-    public String getTermoPesquisa() {
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private EmpresasRepository empresas;
+
+	@Inject
+	private RamoAtividadesRepository ramoAtividadesRepository;
+
+	private List<Empresa> listaEmpresas;
+
+	private String termoPesquisa;
+
+	private Converter ramoAtividadeConverter;
+
+	@Inject
+	private FacesMensages facesMensages;
+
+	public void pesquisar() {
+		listaEmpresas = empresas.pesquisar(termoPesquisa);
+
+		if (listaEmpresas.isEmpty()) {
+			facesMensages.info("Sua consulta não retornou registros.");
+
+		}
+	}
+
+	public void todasEmpresas() {
+		// listaEmpresas = empresas.todas();
+	}
+
+	public List<RamoAtividade> completarRamoAtividade(String termo) {
+		List<RamoAtividade> listaRamoAtividades = ramoAtividadesRepository.pesquisar(termo);
+
+		ramoAtividadeConverter = new RamoAtividadeConverter(listaRamoAtividades);
+
+		return listaRamoAtividades;
+	}
+
+	public List<Empresa> getListaEmpresas() {
+		return listaEmpresas;
+	}
+
+	public String getTermoPesquisa() {
 		return termoPesquisa;
 	}
-    public void setTermoPesquisa(String termoPesquisa) {
+
+	public void setTermoPesquisa(String termoPesquisa) {
 		this.termoPesquisa = termoPesquisa;
 	}
-    
-    public TipoEmpresa[] getTiposEmpresa() {
-    	return TipoEmpresa.values();
-    }
+
+	public TipoEmpresa[] getTiposEmpresa() {
+		return TipoEmpresa.values();
+	}
+	
+	public Converter getRamoAtividadeConverter() {
+		return ramoAtividadeConverter;
+	}
 }
